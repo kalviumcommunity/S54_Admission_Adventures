0e2deaca3eda549da08a661c65a94045ece92585
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-02-03 11:58:01 +0530

    gitignore

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ app.listen(432)
 app.get('/ping',(req,res)=>{
   res.send('pong')
 })
+const express=require('express')
+const app2=express()
+app.listen(432)
+app.get('/ping',(req,res)=>{
+  res.send('pong')
+})
