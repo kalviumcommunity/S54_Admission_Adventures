7c7d6c20cdf8f0a5987a0aed539778d77b1982ee
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-02-04 21:05:16 +0530

    updated server

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const express=require('express')
 const app=express()
-app.listen(432)
-app.get('/ping',(req,res)=>{
-  res.send('pong')
-})
-const express=require('express')
-const app2=express()
-app.listen(432)
+app.listen(3000)
 app.get('/ping',(req,res)=>{
   res.send('pong')
 })
